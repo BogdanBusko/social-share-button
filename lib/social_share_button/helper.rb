04aa52bac1c7f0ed5f9060553a4eb4185fd61d7a
Busko Bogdan <busko.bogdan@gmail.com>
2019-11-12 16:59:09 +0200

    Added title for link

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -12,11 +12,11 @@ module SocialShareButton
       opts[:allow_sites].each do |name|
         extra_data = opts.select { |k, _| k.to_s.start_with?('data') } if name.eql?('tumblr')
         special_data = opts.select { |k, _| k.to_s.start_with?('data-' + name) }
-        
+
         special_data["data-wechat-footer"] = t "social_share_button.wechat_footer" if name == "wechat"
 
         link_title = t "social_share_button.share_to", :name => t("social_share_button.#{name.downcase}")
-        html << link_to("", "#", { :rel => ["nofollow", rel],
+        html << link_to(opts[:link_title], "#", { :rel => ["nofollow", rel],
                                    "data-site" => name,
                                    :class => "ssb-icon ssb-#{name}",
                                    :onclick => "return SocialShareButton.share(this);",
